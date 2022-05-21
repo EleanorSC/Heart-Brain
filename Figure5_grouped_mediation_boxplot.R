84512e669f03a6db2e073c9207897049dce86bb1
EleanorSC <eleanor.conole@ed.ac.uk>
2022-05-21 20:09:35 +0100

    Figure 5 code - tidy / edits to script instructions

--- Figure5_grouped_mediation_boxplot.R
+++ Figure5_grouped_mediation_boxplot.R
@@ -1,12 +1,13 @@
-
-
 ## ---------------------------
 ##
-## Script Purpose: Figures demonstrating microstructural indirect efffect sizes
-##                 I.e how DTI microstructure appears to mediate the VRF-CF association
-##                 (1) Rename variables from supplementary table to abbreviated form
-##                 (2) Create a new column that specifies which hemisphere the tract is assigned to
-##                 (3) Create a plot of tract associations (FA, MD) for both hemispheres
+## Script Purpose: Figure demonstrating Brain IDP and CMR IDP indirect efffect sizes
+##                 Boxplot demonstrating how different features  mediate the VRF-CF association
+##
+##                 (1) Combine both brain IDP SEM analyses and CMR IDP mediation results
+##                 (2) Rename columns for clarity on plot
+##                 (3) Assign boxplot colours for cardiac vs brain IDPs
+##                 (4) Ensure boxplots are ordered in terms of effect size &
+##                     grouped in terms of whether they are cardiac or brain IDPs
 ##
 ##
 ##
@@ -22,16 +23,16 @@ library("ggplot2")
 brain_IDP_plot <- read.csv("supplementary_table11.csv")
 CMR_plot <- read.csv("supplementary_table12.csv")
 
-# Match to column names in supplementary table 11
+# Match to column names in supplementary table 11 -----
 names(CMR_plot)[names(CMR_plot) == "cluster"] <- "category"
 names(CMR_plot)[names(CMR_plot) == "feature"] <- "mediator"
 
-#Combine mediation analyses
+# Combine mediation analyses -----
 Heart_Brain_mediation_plot <- rbind(CMR_plot,
                                     brain_IDP_plot)
 
-# Rename some mediator column metrics for clarity (e.g. fa -> FA and md -> MD)
-Heart_Brain_mediation_plot$category <- str_replace_all(Heart_Brain_mediation_plot$category, 
+# Rename some mediator column metrics for clarity (e.g. fa -> FA and md -> MD) -----
+Heart_Brain_mediation_plot$category <- stringr::str_replace_all(Heart_Brain_mediation_plot$category, 
                                                        c("fa" = "FA", 
                                                          "md" = "MD",
                                                          "icvf" = "ICVF",
@@ -40,29 +41,34 @@ Heart_Brain_mediation_plot$category <- str_replace_all(Heart_Brain_mediation_plo
                                                          "mo" = "MO",
                                                          "volume" = "Volume"))
 
-mycolors <- paletteer_c("grDevices::Blue-Red",18)
-mycolors <- paletteer_c("grDevices::Purple-Blue", 10)
+# NOTE that grDevices::Blue-Red colour palette works well for this figure 
+# Here I have individually assigned hex colours to demonstrate which colour affects which boxplot
+# mycolors <- paletteer::paletteer_c("grDevices::Blue-Red",18) 
 
-mycolors <- (values = c("#023FA5FF",
-                        "#455AA7FF",
-                        "#3C5488B2",
-                        "#6673B1FF",
-                        "#828BBBFF",
-                        "#9BA1C5FF",
-                        # "#4DBBD5B2",
-                        "#8796C2FF",
-                        "#95AFD0FF",
-                        "#A7C6DDFF",
-                        "#7695f0",
+mycolors <- (values = c( # BRAIN IDP COLOUR ASSIGNMENTS  -----
+                         
+                        "#023FA5FF", # MO
+                        "#455AA7FF", # ICVF
+                        "#3C5488B2", # ISOVF
+                        "#6673B1FF", # l2
+                        "#828BBBFF", # MD
+                        "#9BA1C5FF", # OD
+                        "#8796C2FF", # l1
+                        "#95AFD0FF", # FA
+                        "#A7C6DDFF", # L3
+                        "#7695f0",   # Volume (note consider revising- too blue?)
                         
-                        "#DED3D5FF",
-                        "#C8969FFF",
-                        "#BD7B88FF",
-                        "#AF5E70FF",
-                        "#A03C56FF",
-                        "#8E063BFF"
+                        # CARDIAC MRI IDP COLOUR ASSIGNMENTS  -----
+                        
+                        "#DED3D5FF", # Global variance
+                        "#C8969FFF", # Local uniformity
+                        "#BD7B88FF", # Shape
+                        "#AF5E70FF", # Size
+                        "#A03C56FF", # Local Dimness
+                        "#8E063BFF"  # Global intensity
 ))
 
+# Manual reorder note that setting x = reorder(mediator,-coefficient) works just as well -----
 Heart_Brain_mediation_plot  <- Heart_Brain_mediation_plot  %>%
   mutate(category = fct_relevel(category, 
                                 "MO",
@@ -82,12 +88,19 @@ Heart_Brain_mediation_plot  <- Heart_Brain_mediation_plot  %>%
                                 "Local Dimness",
                                 "Global Intensity"))
 
-# Rename some measure column metrics for clarity (e.g. de -> direct effect and ie -> indirect effect)
-Heart_Brain_mediation_plot$measure <- str_replace_all(Heart_Brain_mediation_plot$measure, 
+# Rename some measure column metrics for clarity 
+# (e.g. de -> direct effect and ie -> indirect effect) -----
+Heart_Brain_mediation_plot$measure <- stringr::str_replace_all(Heart_Brain_mediation_plot$measure, 
                                                       c("de" = "direct effect", 
                                                         "ie" = "indirect effect"))
 
 Heart_Brain_mediation_plot$mediator <- as.factor(Heart_Brain_mediation_plot$mediator)
+
+#### Note consider revising still:
+#### (1) gghighlight L thalamic radiation volume
+
+# Boxplot -----
+pd = position_dodge(width = 0.5)
 
 Heart_Brain_mediation_plot %>%
   ggplot(aes(x = coefficient, y = category)) +
@@ -95,42 +108,34 @@ Heart_Brain_mediation_plot %>%
     geom = "errorbar",
     position = pd,
     width = 0.3,
-    # colour = "darkgrey",
+   #  colour = "darkgrey",
     alpha = 0.9,
-    size = 0.3
+    size = 0.6
   ) +
   geom_boxplot(
     aes(fill = category),
-    #alpha =0.4,
     position = pd,
-    # colour = "darkgrey",
+   #  colour = "darkgrey",
     #  alpha = 0.9,
     size = 0.1,
     outlier.shape = NA
   ) +
-  #geom_jitter(aes(colour = category,
-  #                #alpha = mediator)
-  #                alpha = 0.7,
-  #                width = 0.1, 
-  #                height = 0.1)) +
   geom_point(aes(colour = category,
                  alpha = 0.6),
              size = 0.9, 
              position = position_jitter()) +
-  # position = "jitter"
-  # ) +
   theme(legend.position = "none") +
   scale_fill_manual(values = mycolors) +
   scale_colour_manual(values = mycolors) +
   theme_bw() +
   theme(legend.position = "none") +
-  #guides(fill = FALSE) +
   theme(axis.text.x = element_text(size = 9),
         axis.text.y = element_text(size = 9), 
         axis.title.x = element_text(face = "bold", size = 11), 
         axis.title.y = element_text(face = "bold", size = 11)) +
   # coord_flip ()+
-  # Write the axis
   xlab("") +
   ylab("") +
   facet_wrap(~measure)
+
+### END
